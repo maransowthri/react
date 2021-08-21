5d6c24452698081c42335f3d18d10cb9d53247eb
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2021-08-21 12:57:28 +0530

    Finished chakra ui project

---
+++ new_course/chakra-app/pages/index.tsx
@@ -0,0 +1,39 @@
+import {
+  Button,
+  Flex,
+  Heading,
+  Input,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+const IndexPage = () => {
+  const { toggleColorMode } = useColorMode();
+  const formBackground = useColorModeValue("gray.100", "gray.700");
+
+  return (
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Flex
+        direction="column"
+        background={formBackground}
+        padding={12}
+        rounded={6}
+      >
+        <Heading mb={6}>Log in</Heading>
+        <Input
+          placeholder="johndoe@gmail.com"
+          variant="filled"
+          mb={3}
+          type="email"
+        />
+        <Input placeholder="******" variant="filled" mb={6} type="password" />
+        <Button mb={6} colorScheme="teal">
+          Log in
+        </Button>
+        <Button onClick={toggleColorMode}>Switch Color Mode</Button>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default IndexPage;
